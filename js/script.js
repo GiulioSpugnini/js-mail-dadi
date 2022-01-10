b4f3492f2ab2dd08d168ea5a574c32a38236f514
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-01-10 16:04:33 +0100

    implemented 1st, 2nd and 3rd steps

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ let emails = ['alberto@gmail.com', 'bertoldo@yahoo.com', 'giulio@yahoo.com'];
 console.log(emails);
 
 //Chiedo all'utente la sua email
-userMail = prompt("Inserisci la tua email");
+userMail = prompt("Inserisci la tua email").trim();
 console.log(userMail);
 if (!isNaN(userMail) || userMail == null) {
     alert("Hai inserito l'input sbagliato, ti prego di riprovare")
@@ -39,3 +39,16 @@ Gioco dei Dadi
 3.Controllo qual è più alto e gestisco eventuali pareggi
 4.Stampo in console i tiri del dado e del risultato
 */
+
+//Genero un numero random da 1 a 6 per cpu
+const cpuNumber = Math.random(6);
+//Genero un numero random da 1 a 6 per l'utente
+const userNumber = Math.random(6);
+//Controllo qual è più alto e gestisco eventuali pareggi
+if (cpuNumber == userNumber) {
+    console.log('PAREGGIO');
+} else if (cpuNumber > userNumber) {
+    console.log('CPU HA VINTO !')
+} else if (cpuNumber < userNumber) {
+    console.log('L UTENTE HA VINTO !')
+}
