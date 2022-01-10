7f66888e31a7e72704c003803bbccc0da21c6367
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-01-10 15:21:49 +0100

    added steps to do in the es Mail

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log('OK js!');
+/*
+MAIL
+
+1.Creo un array di email
+2.Chiedo all'utente la sua email
+3.Controllo che la sua email sia presente nell' array
+4.Stampo la risposta in base al risultato del controllo
+*/
